36b9778cfc28de5f009f3a0ee333d4a0be707fd4
triskofwhaleisland <triskofwhaleisland@gmail.com>
2023-08-06 14:31:45 -0400

    Client: first pass: now uses RefCell, no more state storage

--- examples/all_dispatches.rs
+++ examples/all_dispatches.rs
@@ -8,13 +8,13 @@ use std::error::Error;
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv::dotenv()?;
     let user_agent = std::env::var("USER_AGENT")?;
-    let (client, mut client_state) = Client::new(user_agent)?.with_default_state();
+    let client = Client::new(user_agent);
     eprintln!("Made client!");
 
     let target_nation = "Testlandia";
     let request = PublicNationRequest::new(target_nation, &[DispatchList]);
     // eprintln!("{request}");
-    let raw_response = client.get(request, &mut client_state).await?;
+    let raw_response = client.get(request).await?;
     let text = raw_response.text().await?;
     // eprintln!("{text}");
     let response = Nation::from_xml(&text)?;
